ca3165ae2b83fddde7b64af12f9a28b06ff8c76e
Pierre-Andre Saulais <pasaulais@free.fr>
2018-04-08 03:39:28 +0100

    Add a sketch to test the display

---
+++ TestLCD/TestLCD.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup()   {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  // text display tests
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.print("0x"); display.println(0xDEADBEEF, HEX);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+}
+
+void loop() {
+  
+}
